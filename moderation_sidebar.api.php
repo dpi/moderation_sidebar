49b367a96079314bac966d2666132b9a0432b537
git <git@3563293.no-reply.drupal.org>
2018-03-07 08:48:26 -0800

    Issue #2950875 by drupalgin: Fixed coding standards

--- moderation_sidebar.api.php
+++ moderation_sidebar.api.php
@@ -11,12 +11,14 @@
  */
 
 /**
- * Alter the information provided by \Drupal\moderation_sidebar\Controller\ModerationSidebarController::sideBar()
+ * Alter the build rendered in the moderation sidebar.
  *
  * @param array $build
  *   The render array for the sidebar.
  * @param \Drupal\Core\Entity\EntityInterface $entity
  *   The default or latest revision of an entity being moderated.
+ *
+ * @see \Drupal\moderation_sidebar\Controller\ModerationSidebarController::sideBar()
  */
 function hook_moderation_sidebar_alter(array &$build, \Drupal\Core\Entity\EntityInterface $entity) {
   if ($entity->getEntityTypeId() === 'my_great_type') {
