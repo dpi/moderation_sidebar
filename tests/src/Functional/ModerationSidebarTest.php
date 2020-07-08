1cca5de0dfae505fb07d1b6c085be5298285129b
dpi <pro@danielph.in>
2020-07-08 15:29:30 +0800

    14: adds the generic test from 3157633

---
+++ tests/src/Functional/ModerationSidebarTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\Tests\moderation_sidebar\Functional;
+
+use Drupal\entity_test\Entity\EntityTestMulRevPub;
+use Drupal\Tests\BrowserTestBase;
+use Drupal\Tests\content_moderation\Traits\ContentModerationTestTrait;
+
+/**
+ * Tests basic behaviour of Moderation Sidebar using a test entity.
+ *
+ * @group moderation_sidebar
+ */
+class ModerationSidebarTest extends BrowserTestBase {
+
+  use ContentModerationTestTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'moderation_sidebar',
+    'toolbar',
+    'content_moderation',
+    'workflows',
+    'entity_test',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $workflow = $this->createEditorialWorkflow();
+    $this->addEntityTypeAndBundleToWorkflow($workflow, 'entity_test_mulrevpub', 'entity_test_mulrevpub');
+
+    $this->drupalLogin($this->createUser([
+      'view test entity',
+      'access toolbar',
+      'access toolbar',
+      'use ' . $workflow->id() . ' transition create_new_draft',
+      'use ' . $workflow->id() . ' transition archive',
+      'use ' . $workflow->id() . ' transition publish',
+      'use moderation sidebar',
+    ]));
+  }
+
+  /**
+   * Test toolbar item appears.
+   */
+  public function testToolbarItem() {
+    $entity = EntityTestMulRevPub::create([
+      'name' => $this->randomMachineName(),
+    ]);
+    $entity->save();
+    $this->drupalGet($entity->toUrl());
+
+    // Make sure the button is where we expect it.
+    $toolbarItem = $this->assertSession()->elementExists('css', '.moderation-sidebar-toolbar-tab a');
+    // Make sure the button has the right attributes.
+    $this->assertEquals(sprintf('/moderation-sidebar/entity_test_mulrevpub/%s/latest', $entity->id()), $toolbarItem->getAttribute('href'));
+    $this->assertEquals('Tasks', $toolbarItem->getText());
+  }
+
+}
